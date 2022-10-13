3b0b097a683d26b5d900a9b969f48f84935d1442
KceroStudios <kcerostudios@gmail.com>
2022-10-13 16:47:30 -0400

    diseño de la pokedex concluido2

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,7 +1,6 @@
 import './App.css'
 import React from 'react'
 import Pokedex from './components/Pokedex'
-import pokedex from './assets/images/pokedex-referencia.jpg'
 import CenterBar from './components/CenterBar'
 import PokedexBody from './components/PokedexBody'
 import PoquedexCover from './components/PokedexCover'
@@ -13,8 +12,8 @@ function App() {
       <PokedexBody></PokedexBody>
       <CenterBar />
       <PoquedexCover></PoquedexCover>
-      <img width={600} src={pokedex} alt="pokedex" />
     </Pokedex>  
+    
   )
 }
 
